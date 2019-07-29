3dcbab7317216e7d91f0edd707bd3e8431292a87
unknown <ch.paronda@gmail.com>
2019-07-29 22:47:08 +0800

    3rd change

--- src/components/vueHome.vue
+++ src/components/vueHome.vue
@@ -4,30 +4,53 @@
             <span class="heading">
                 Hello! This is
             </span>
-            <h1 class="mb-0 ">Clyde Harold <span class="text-primary">Paronda</span></h1> 
+            <h1 class="mb-0">Clyde Harold <span class="text-primary">Paronda</span></h1> 
             <div class="subheading mb-5" style='font-family: "Saira Extra Condensed", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji" !important'>
                 &lt; Instructor / Developer  &gt;
             </div> 
-            <p class="lead mb-5" style="text-align:justify">    
+            
+            <p class="lead" style="text-align:justify;margin-bottom:1em !important">    
                 A service-oriented individual with proven skills in teaching Web, Database and Android 
                 Development to students. 
                 Striving to teach course information, as well as critical thinking and success-building skills.  
+                
             </p> 
-            <div class="social-icons">
-                <a href="https://linkedin.com/in/clydeharold" target="_blank">
-                    <i class="fab fa-linkedin-in"></i>
-                </a> 
-                <a href="https://twitter.com/clydeharold" target="_blank">
-                    <i class="fab fa-twitter"></i>
-                </a> 
-                <a href="https://www.facebook.com/clydeharold" target="_blank">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-            </div>
+            <div class="row" >
+                <div class="col">
+                    <h3>Skills</h3>
+                        <ul class="subheading">
+                            <li>HTML, CSS, Vue.js, Angular</li>
+                            <li>Database Design and Development</li>
+                            <li>Android Development</li>
+                            <li>Microsoft Office Tools</li>
+                            <li>PC Troubleshooting</li>
+                        </ul>
+                </div>
+                <div class="col">
+                    <div class="social-icons">
+                        <a href="https://linkedin.com/in/clydeharold" target="_blank">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a> 
+                        <a href="https://twitter.com/clydeharold" target="_blank">
+                            <i class="fab fa-twitter"></i>
+                        </a> 
+                        <a href="https://www.facebook.com/clydeharold" target="_blank">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </div>
+                </div>
+    </div>
+            
+                
         </div>
     </section>
 </template>
 <style>
+    h3 {
+        font-size:1.5rem;
+        font-family: "Saira Extra Condensed", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        font-weight: 700;
+    }
     h1 {
         font-size:4rem;
         font-family: "Saira Extra Condensed", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
--- src/components/vueSideBar.vue
+++ src/components/vueSideBar.vue
@@ -1,70 +1,66 @@
 <template>
   <nav id="sidebar" class="">
-            <div class="sidebar-header"> 
-                <strong>CH</strong>
-                <span class="d-none d-lg-block">
-                    <img src="../assets/images/clydepic.png" alt="clydeharold" class="img-fluid img-profile rounded-circle mx-auto mb-2">
-                </span>
-                <h3>@clydeharold</h3>
-            </div>
-
-            <ul class="list-unstyled components">
-                <li class="active">
-                    <router-link to='/' tag="li" active-class="active" exact>
-                       <a>
-                            <svg class="svg-inline--fa fa-home fa-w-18" aria-hidden="true" data-prefix="fas" data-icon="home" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M488 312.7V456c0 13.3-10.7 24-24 24H348c-6.6 0-12-5.4-12-12V356c0-6.6-5.4-12-12-12h-72c-6.6 0-12 5.4-12 12v112c0 6.6-5.4 12-12 12H112c-13.3 0-24-10.7-24-24V312.7c0-3.6 1.6-7 4.4-9.3l188-154.8c4.4-3.6 10.8-3.6 15.3 0l188 154.8c2.7 2.3 4.3 5.7 4.3 9.3zm83.6-60.9L488 182.9V44.4c0-6.6-5.4-12-12-12h-56c-6.6 0-12 5.4-12 12V117l-89.5-73.7c-17.7-14.6-43.3-14.6-61 0L4.4 251.8c-5.1 4.2-5.8 11.8-1.6 16.9l25.5 31c4.2 5.1 11.8 5.8 16.9 1.6l235.2-193.7c4.4-3.6 10.8-3.6 15.3 0l235.2 193.7c5.1 4.2 12.7 3.5 16.9-1.6l25.5-31c4.2-5.2 3.4-12.7-1.7-16.9z"></path></svg><!-- <i class="fas fa-home"></i> -->
-                        Home
-                       </a>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link to='/About' tag="li" active-class="active">
-                        <a>
-                            <svg class="svg-inline--fa fa-user-circle fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="briefcase" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M320 288h192v144c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V288h192v20c0 6.627 5.373 12 12 12h104c6.627 0 12-5.373 12-12v-20zm192-112v80H0v-80c0-26.51 21.49-48 48-48h80V80c0-26.51 21.49-48 48-48h160c26.51 0 48 21.49 48 48v48h80c26.51 0 48 21.49 48 48zM320 96H192v32h128V96z"></path></svg><!-- <i class="fas fa-briefcase"></i> -->
-                            About
-                        </a>
-                         
-                    </router-link>
-                </li>
-                <li>
-                  <router-link to="/Education" tag="li" active-class="active">
+        <div class="sidebar-header"> 
+            <strong>CH</strong>
+            <span class="d-none d-lg-block">
+                <img src="../assets/images/clydepic.png" alt="clydeharold" class="img-fluid img-profile rounded-circle mx-auto mb-2">
+            </span>
+            <h3>@clydeharold</h3>
+        </div>
+        <ul class="list-unstyled components" style="padding:5px;">
+            <router-link to='/' tag="li" active-class="active" exact>
+                <a>
+                <i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp;Home
+                </a>
+            </router-link>
+            <router-link to='/About' tag="li" active-class="active">
+                <a>
+                    <i class="fa fa-user" aria-hidden="true"></i>&nbsp;About
+                </a>   
+            </router-link>
+            <router-link to="/Education" tag="li" active-class="active">
+                <a>
+                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>&nbsp;Education
+                </a>
+            </router-link>
+            <li>
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <i class="fa fa-wrench" aria-hidden="true"></i>
+                    Experiences
+                </a>
+            </li>
+            
+            <ul class="collapse list-unstyled" id="homeSubmenu">
+                <router-link to='/Work' tag="li" active-class="active">
                     <a>
-                         <svg class="svg-inline--fa fa-graduation-cap fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="briefcase" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M320 288h192v144c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V288h192v20c0 6.627 5.373 12 12 12h104c6.627 0 12-5.373 12-12v-20zm192-112v80H0v-80c0-26.51 21.49-48 48-48h80V80c0-26.51 21.49-48 48-48h160c26.51 0 48 21.49 48 48v48h80c26.51 0 48 21.49 48 48zM320 96H192v32h128V96z"></path></svg><!-- <i class="fas fa-briefcase"></i> -->
-                      Education
+                        <i class="fa fa-briefcase" aria-hidden="true"></i>&nbsp;Work
+                    </a> 
+                </router-link>
+                <router-link to='/Training' tag="li" active-class="active">
+                    <a>
+                        <i class="fa fa-certificate" aria-hidden="true"></i>&nbsp;Training
                     </a>
-                  </router-link>
-                </li>
-                 <li>
-                    <router-link to='/Work' tag="li" active-class="active">
-                       <a>
-                             <svg class="svg-inline--fa fa-briefcase fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="briefcase" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M320 288h192v144c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V288h192v20c0 6.627 5.373 12 12 12h104c6.627 0 12-5.373 12-12v-20zm192-112v80H0v-80c0-26.51 21.49-48 48-48h80V80c0-26.51 21.49-48 48-48h160c26.51 0 48 21.49 48 48v48h80c26.51 0 48 21.49 48 48zM320 96H192v32h128V96z"></path></svg><!-- <i class="fas fa-briefcase"></i> -->
-                        Work
-                       </a>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link to='/Training' tag="li" active-class="active">
-                       <a>
-                             <svg class="svg-inline--fa fa-briefcase fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="briefcase" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M320 288h192v144c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V288h192v20c0 6.627 5.373 12 12 12h104c6.627 0 12-5.373 12-12v-20zm192-112v80H0v-80c0-26.51 21.49-48 48-48h80V80c0-26.51 21.49-48 48-48h160c26.51 0 48 21.49 48 48v48h80c26.51 0 48 21.49 48 48zM320 96H192v32h128V96z"></path></svg><!-- <i class="fas fa-briefcase"></i> -->
-                        Training
-                       </a>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link to='/Seminar' tag="li" active-class="active">
-                       <a>
-                             <svg class="svg-inline--fa fa-briefcase fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="briefcase" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M320 288h192v144c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V288h192v20c0 6.627 5.373 12 12 12h104c6.627 0 12-5.373 12-12v-20zm192-112v80H0v-80c0-26.51 21.49-48 48-48h80V80c0-26.51 21.49-48 48-48h160c26.51 0 48 21.49 48 48v48h80c26.51 0 48 21.49 48 48zM320 96H192v32h128V96z"></path></svg><!-- <i class="fas fa-briefcase"></i> -->
-                        Seminar
-                       </a>
-                    </router-link>
-                </li>
-               
-                <li>
-                    <a href="#">
-                        <svg class="svg-inline--fa fa-paper-plane fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="paper-plane" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"></path></svg><!-- <i class="fas fa-paper-plane"></i> -->
-                        Contact
+                </router-link>
+                <router-link to='/Seminar' tag="li" active-class="active">
+                    <a>
+                        <i class="fa fa-address-card-o" aria-hidden="true"></i>&nbsp;Seminar
                     </a>
-                </li>
+                </router-link>    
             </ul>
-        </nav>
+            <li>
+                <a href="#">
+                    <i class="fa fa-paper-plane" aria-hidden="true"></i>&nbsp;Contact
+                </a>
+            </li>
+        </ul>
+    </nav>
 </template>
+
+<style>
+ul ul a {
+    background-color:#17a2b8 !important;
+}
+ul ul a:hover {
+    background-color:white;
+}
+</style>
